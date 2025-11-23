f0a026b07872175c87cd9ef1e159dc7cac9e7ea0
LolTaun <loltaun001@gmail.com>
2025-11-23 16:46:38 +0300

    Added comments to hello.py

--- hello.py
+++ hello.py
@@ -8,11 +8,12 @@ def main(
     """
     Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
     """
-    if formal:
+    if formal:  # Если выбран формальный стиль флагом --formal или -f
         print(f"Добрый день, {name} {lastname}!")
     else:
         print(f"Привет, {name}!")
 
 if __name__ == "__main__":
+    # Запуск приложения Typer
     typer.run(main)
 
